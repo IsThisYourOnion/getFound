960cc84743f4835295fd684f209245a50ba39551
adam-kirstein <adamnkirstein@gmail.com>
2023-06-22 16:20:27 -0700

    aint nobody got time for import errors with utils. removed utils folder and moved utils.py to scrapers directory

--- getFound/scrapers/main.py
+++ getFound/scrapers/main.py
@@ -1,16 +1,15 @@
-from linkedinURLSkimmer import LinkedinUrlSkimmer
 from linkedInScraper import LinkedInScraper
-from ..utils.utils import JSONDataProcessor
+from utils import JSONDataProcessor
 import os
 from itertools import chain
 
 
 def main():
-    ## Collect URLs (HREFS) for jobs from specified positions
-    scraper = LinkedinUrlSkimmer()
-    job_positions = ['deep learning scientist', 'data scientist', 'machine learning engineer']
-    scraper.scrape(job_positions, 20000, 8)  # Specify the job positions, number of iterations, and number of threads
-    # will stop at 20k hrefs per job item
+    # ## Collect URLs (HREFS) for jobs from specified positions
+    # scraper = LinkedinUrlSkimmer()
+    # job_positions = ['deep learning scientist', 'data scientist', 'machine learning engineer']
+    # scraper.scrape(job_positions, 20000, 8)  # Specify the job positions, number of iterations, and number of threads
+    # # will stop at 20k hrefs per job item
 
     # Read in hrefs to extract job descriptions from Utils functions
     base_project_dir = JSONDataProcessor.get_root_path()
@@ -18,7 +17,6 @@ def main():
     processor = JSONDataProcessor(directory_path)
     all_data = processor.read_json_files()
     href_list = list(chain.from_iterable(all_data))
-
 
     scraper = LinkedInScraper(href_list)
     scraper.scrape_all()
