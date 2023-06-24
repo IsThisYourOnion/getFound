a1d7e9c5dde2bf1308e1ee6a1caee516be10818a
adam-kirstein <adamnkirstein@gmail.com>
2023-06-24 09:32:15 -0700

    imports

--- getFound/src/main/main.py
+++ getFound/src/main/main.py
@@ -1,8 +1,8 @@
-from getFound.src.modules.linkedinJobSkimmer_V2 import collect_job_links
+from getFound.src.modules.linkedinJobSkimmer_V2 import main_job_links
 from getFound.src.modules.jobDetailExtractor import runJobManager
 from getFound.src.modules.textProcessor import runJobProcessor
 from getFound.src.modules.keywordExtractor import keywords
-
+from getFound.src.modules.gptExtractor import GPTKeywords
 
 
 
@@ -12,7 +12,7 @@ def main():
     """
 
     print('\n[Phase 1] Initiating LinkedIn job link collection...')
-    collect_job_links()
+    main_job_links()
 
     print('[Phase 2] Starting job data extraction...')
     runJobManager()
@@ -21,7 +21,7 @@ def main():
     runJobProcessor()
 
     print('[Phase 4] Extracting keywords from job data...')
-    keywords()
+    GPTKeywords()
 
 
 if __name__ == "__main__":
