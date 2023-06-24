df4c703d4ad965c81bfb5f5cbc5a14a820003406
adam-kirstein <adamnkirstein@gmail.com>
2023-06-24 09:33:26 -0700

    updates

--- getFound/src/config/params.py
+++ getFound/src/config/params.py
@@ -1,19 +1,22 @@
 # params.py
 
 # List of search terms
-search_terms = ['deep learning scientist']
-                # 'AI engineer',
-                # 'AI Researcher',
-                # 'Applied Scientist',
-                # 'data scientist']
+search_terms = ['deep learning scientist',
+                'AI engineer',
+                'AI Researcher',
+                'Applied Scientist',
+                'data scientist']
 
 
 
 # User's email
-email = "EMAIL"
+email = EMAIL
 
 # User's password
 # WARNING: Storing passwords in plain text is not secure.
-password = "PASSWORD"
+password = PASS
 
-num_jobs = 10
+num_jobs = 5
+
+gpt_prompt = 'can you first extract keyphrases, produce attractive resume bullet points, output in a json format with Keyphrases and Bullet points being the keys based on the text:'
+open_ai_key = KEY
