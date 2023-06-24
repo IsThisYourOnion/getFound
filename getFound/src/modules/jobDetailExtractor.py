2019d6a20b5825cae472a060b6a639d9ac674ab7
adam-kirstein <adamnkirstein@gmail.com>
2023-06-24 08:21:11 -0700

    threading

--- getFound/src/modules/jobDetailExtractor.py
+++ getFound/src/modules/jobDetailExtractor.py
@@ -2,11 +2,14 @@ from linkedin_api import Linkedin
 from getFound.src.utils.utils import DataManager
 import re
 from getFound.src.config import params
+from concurrent.futures import ThreadPoolExecutor
+import time
 
 class JobManager:
-    def __init__(self, email, password):
+    def __init__(self, email, password, max_workers=10):
         self.manager = DataManager()
         self.api = Linkedin(email, password)
+        self.max_workers = max_workers
 
     def clean_job_ids(self, job_links):
         pattern = re.compile(r'-?(\d{10})\?')
@@ -18,12 +21,14 @@ class JobManager:
                 job_ids.append(match.group(1))
         return job_ids
 
-    def pull_linkedin_data(self, job_ids):
-        for job in job_ids:
-            profile = self.api.get_job(job)
-            file_name = f'job_{job}'
-            self.manager.write_data('linked_job_data', file_name, 'json', profile)
+    def _get_and_write_job_data(self, job):
+        profile = self.api.get_job(job)
+        file_name = f'job_{job}'
+        self.manager.write_data('linked_job_data', file_name, 'json', profile)
 
+    def pull_linkedin_data(self, job_ids):
+        with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
+            executor.map(self._get_and_write_job_data, job_ids)
 
 def runJobManager():
     # Usage example:
